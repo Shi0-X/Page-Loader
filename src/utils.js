4b6977c3b906aac5649205ab6c53060039fa007b
Shi0-X <jshi0x@gmail.com>
2025-01-30 16:50:26 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,14 @@
 import path from 'path';
 
 // 🔹 Normaliza nombres eliminando caracteres no válidos
-const processName = (name, replacer = '-') => name
-  .replace(/[?&=]/g, '')  // Elimina caracteres problemáticos para nombres de archivos
-  .match(/\w+/gi)         // Extrae partes alfanuméricas del string
-  .join(replacer);        // Une los valores con el `replacer`
+const processName = (name, replacer = '-') => name.match(/\w*/gi)  // Extrae partes alfanuméricas del string
+  .filter((x) => x)   // Filtra valores vacíos
+  .join(replacer);    // Une los valores con el `replacer`
 
 // 🔹 Convierte una URL en un nombre de archivo seguro (ej. "example-com-path-to-page.html")
 export const urlToFilename = (link, defaultFormat = '.html') => {
   const { dir, name, ext } = path.parse(link);
-  const slug = processName(path.join(dir, name));
+  const slug = processName(dir + name);
   const format = ext || defaultFormat;
 
   return `${slug}${format}`;
@@ -17,8 +16,8 @@ export const urlToFilename = (link, defaultFormat = '.html') => {
 
 // 🔹 Convierte una URL en un nombre de directorio seguro (ej. "example-com-path-to-page_files")
 export const urlToDirname = (link, postfix = '_files') => {
-  const { dir, name, ext } = path.parse(link);
-  const slug = processName(path.join(dir, name, ext));
+  const { dir, name } = path.parse(link);
+  const slug = processName(dir + name);
 
   return `${slug}${postfix}`;
 };
